8540a99b1037644a2f2fcb97508041abef20313a
GuYun-D <1946306647@qq.com>
2023-07-31 10:03:03 +0800

    style: 代码格式化

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,18 +4,18 @@ const request = new Request({
   baseURL: 'http://localhost:3000',
   timeout: 10000,
   interceptors: {
-    requestSuccess(config) {
+    requestSuccess (config) {
       return config
     },
-    requestFail(err) {
+    requestFail (err) {
       console.log('请求失败', err)
     },
-    responseSuccess(res) {
+    responseSuccess (res) {
       // TDDO: 后端服务未启动时的错误
       console.log('响应成功', res)
       return res
     },
-    responseFail(err) {
+    responseFail (err) {
       console.log('响应失败', err)
     }
   }
