4ff625d1cd613b46c5ccf678dec18f7152e31175
GuYun-D <1946306647@qq.com>
2023-08-18 14:52:16 +0800

    feat: 添加element-plus国际化切换

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view></router-view>
+  <ElConfigProvider :locale="language">
+    <router-view></router-view>
+  </ElConfigProvider>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+import enLanguagePackage from 'element-plus/lib/locale/lang/en'
+import zhLanguagePackage from 'element-plus/lib/locale/lang/zh-cn'
+import useApp from '@/stores/modules/app'
+
+const app = useApp()
+const language = computed(() => {
+  return app.language === 'en' ? enLanguagePackage : zhLanguagePackage
+})
+</script>
